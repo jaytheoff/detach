2db5dda332450962cbd225b3a9b182557fb8c4ff
Jean Junior L <bronzeoffjay123@gmail.com>
2026-03-06 17:44:00 -0500

    [26-3-6] tired to make mechanic
    we canceled our old idea

---
+++ Scripts/Global.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var player_pos_x : float = 0
+var player_pos_y : float = 0
+var player_health : int = 100
+var player_score : int = 0
+
+var soul_pos_x : int = 0
+var soul_pos_y : int = 0
+
+var player_mode : String = "Default" # can be "normal", "soul"
